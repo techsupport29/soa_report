99b473c41d7c822da4acf60cea639e51042a5a83
brylle <kapnudel007@gmail.com>
2022-03-29 15:46:00 +0800

    zipping

--- app/Http/Controllers/API/EmailController.php
+++ app/Http/Controllers/API/EmailController.php
@@ -124,6 +124,7 @@ class EmailController extends Controller
         // $image = str_replace(' ', '+', $image);
 
         // foreach ($request->data['email_details'] as $email){
+        
 
         $data = [
             // 'email' => $email['email'],
